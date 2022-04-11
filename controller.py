e50d1d755593c97ab3de86ba04eb4ba3973b2a4a
Pavel-L-40 <mr.l.piranya@gmail.com>
2022-04-11 12:01:19 +0300

    created controller

---
+++ controller.py
@@ -0,0 +1,8 @@
+import racional_num as model
+import view
+
+def button_click():
+    value_a = view.get_value()
+    value_b = view.get_value()
+    model.init(value_a, value_b)
+    result = model
